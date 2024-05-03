ae79b617ec9c06b95d775dc9daf00aa9bdb6b7a1
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-03 08:55:59 -0400

    Update Same_Tree.py with an optimized version in comments
    After reviewing other solutions I found a much better runtime solution that looks pretty similar to my own but cleaned up some.  This version has been added at the end in comment code.

--- Same_Tree.py
+++ Same_Tree.py
@@ -17,3 +17,16 @@ class Solution:
             return False
         #recursively compare values
         return (Solution().isSameTree(p.left, q.left) and Solution().isSameTree(p.right, q.right))
+        
+#################################
+#A cleaned up version from the solutions that has a much better runtime
+'''
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q: # identify the p and q are not null
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return (self.isSameTree(p.left, q.left) and
+                self.isSameTree(p.right,q.right))
+'''
