2dd5b652097cbf532fd0ca48a7e2d8b61d88c0f3
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-16 09:10:04 -0400

    Create Valid_Palindrome.py
    Python solution to Valid Palindrome challenge on Leetcode.com.

---
+++ Valid_Palindrome.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #create new empty string
+        newS = ""
+        #remove spaces and non-alphanumeric characters with isalnum
+        for i in s:
+            if i.isalnum() and i != " ":
+                newS += i
+        #force the string to all lowercase for easier comparisons
+        newS = newS.lower()
+        #create left and right side pointers
+        left = 0
+        right = len(newS)-1
+        #create true boolean flag to track palindrome status
+        isPalindrome = True
+        #loop to the middle
+        while left <= right:
+            #check if left and right are different
+            if newS[left] != newS[right]:
+                #if different end and set flag to false
+                isPalindrome = False
+                break
+            #otherwise continut to next pair of characters
+            right -= 1
+            left += 1
+        #return boolean flag
+        return isPalindrome
+
+'''After looking through other solutions I forgot Python can move backwards
+using a negative slice.  My code could have been cleaned up with something like
+return newS == newS[::-1]
+'''
