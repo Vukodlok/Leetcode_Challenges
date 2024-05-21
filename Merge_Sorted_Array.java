80c48bcb74394bc0609a391ba982b20a4a82605d
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-21 13:39:51 -0400

    Update Merge_Sorted_Array.java
    Modified comment code documentation to a more official format.

--- Merge_Sorted_Array.java
+++ Merge_Sorted_Array.java
@@ -1,4 +1,23 @@
+/* 
+* Leetcode challenge: Merge Sorted Array
+*
+* You are given two integer arrays nums1 and nums2, sorted in ascending order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+* Merge nums1 and nums2 into a single array sorted in ascending order.
+* The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+* To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
+* and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+*
+* Created: 5/13/24
+* Author: Mark Robuck
+*/
 class Solution {
+    /*
+    * Merge two sorted arrays back into the first array, in ascending order
+    @param int[] nums1, sorted array of m elements followed by n elements set to zero
+    @param int[] nums2, sorted array of n elements
+    @param int m, number of sorted elements at the front portion of nums1
+    @param int n, number of sorted elements in nums2, and the number of empty 0 elements at the back portion of nums1
+    */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         //set pointers
         int i = m - 1;
