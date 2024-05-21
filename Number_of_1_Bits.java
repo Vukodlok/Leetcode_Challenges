2afea29318b15c607aaf61dcf579ae193890a105
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-21 10:09:38 -0400

    Create Number_of_1_Bits.java
    Java solution for Number of 1 Bits challenge on Leetcode.com.

---
+++ Number_of_1_Bits.java
@@ -0,0 +1,20 @@
+/*
+* Leetcode challenge: Number of 1 Bits
+*
+* Write a function that takes the binary representation of a positive integer
+* and returns the number of set bits it has (also known as the Hamming weight).
+*
+* Created 5/21/24
+* Author: Mark Robuck
+*/
+
+class Solution{
+    /*
+    * Find the hamming weight of an integer n, n is assumed to be a positive integer
+    @param n, positive integer to have hamming weight calculated of
+    @return int, hamming weight of integer n
+    */
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+}
