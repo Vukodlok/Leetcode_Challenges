c0934c29ed360be475e82ddab1402dad1363c456
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 10:30:50 -0400

    Update Ransom_Note.java
    Update documentation comment code.

--- Ransom_Note.java
+++ Ransom_Note.java
@@ -1,6 +1,22 @@
+/*
+* Leetcode Challenge: Ransom Note
+* 
+* Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+* 
+* Each letter in magazine can only be used once in ransomNote.
+* 
+* 4/30/24
+* Mark Robuck
+*/
 import java.util.ArrayList;
 
 class Solution {
+    /*
+    * Determine if all characters in a string are contained within another string.
+    * @params ransomNote, the string to be tested if all character are also within magazine
+    * @params magazine, the string to be used to test if all characters from ransomNote are within it
+    * @return true if all characters in ransomNote are within magazine, and false if not
+    */
     public boolean canConstruct(String ransomNote, String magazine) {
         boolean containsAll = true;
         //create an array of letters for each string
