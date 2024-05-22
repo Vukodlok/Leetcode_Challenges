0fd1c190b1a13e813d4de310839c5c676a410287
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 12:30:22 -0400

    Update Reverse_Bits.py
    Update documentation comment code.

--- Reverse_Bits.py
+++ Reverse_Bits.py
@@ -1,7 +1,21 @@
-#note: I originally thought the directions asked to flip all 0's and 1's 
-#like a bitwise not.
+"""
+Leetcode Challenge: Reverse Bits
+
+Reverse bits of a given 32 bits unsigned integer.
+
+5/7/24
+Mark Robuck
+"""
+#note: I originally thought the directions asked to flip all 0's and 1's like a bitwise not.
 # That is not the case, this challenge wants the binary number returned in reverse order.
 class Solution:
+    """
+    Reverse the order of the bits in a binary number
+    Args:
+        n(int): number to be reverse
+    Returns:
+        int: order of bits reversed from n
+    """
     def reverseBits(self, n: int) -> int:
         binary = bin(n)
         reverse = 0
