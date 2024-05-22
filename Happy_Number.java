cf2eaa3bb308be3da75997782b16e0e2e99cc084
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 10:41:11 -0400

    Update Happy_Number.java
    Update documentation comment code.

--- Happy_Number.java
+++ Happy_Number.java
@@ -1,3 +1,18 @@
+/*
+* Leetcode Challenge: Happy Number
+* 
+* Write an algorithm to determine if a number n is happy.
+* 
+* A happy number is a number defined by the following process:
+* 
+* Starting with any positive integer, replace the number by the sum of the squares of its digits.
+* Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+* Those numbers for which this process ends in 1 are happy.
+* Return true if n is a happy number, and false if not.
+* 
+* 5/20/24
+* Mark Robuck
+*/
 import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.List;
@@ -5,6 +20,11 @@ import java.util.List;
 class Solution {
     public static List<Integer> digits = new ArrayList<>();
     public static int result;
+    /*
+    * Repeat testing of an integer until the result is either 1 or an infinite loop.
+    * @params n, integer to be tested
+    * @return true if n is a happy number by the given definition, false if not a happy number
+    */
     public boolean isHappy(int n) {
         //used to store current iteration of happy number calculation
         result = n;
@@ -23,6 +43,10 @@ class Solution {
         }
         return result == 1;
     }
+    /*
+    * Split an integer into individual digits
+    * @params num, integer to be split
+    */
     public void splitDigits(int num) {
         digits.clear();
         //split num into individual digits and add to the set digits
@@ -32,6 +56,9 @@ class Solution {
             num /= 10;
         }
     }
+    /*
+    * Calculate the sum of the squares of all digits in a list
+    */
     public void calcHappy() {
         //perform squaring and sum
         for (int num : digits){
