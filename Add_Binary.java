2765063b1b5682f6e58753fc2c2c3a0bd11f709b
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 12:21:38 -0400

    Update Add_Binary.java
    Update documentation comment code.

--- Add_Binary.java
+++ Add_Binary.java
@@ -1,6 +1,20 @@
+/*
+* Leetcode Challenge: Add Binary
+*
+* Given two binary strings a and b, return their sum as a binary string.
+*
+* 4/29/24
+* Mark Robuck
+*/
 import java.math.BigInteger;
 
 class Solution {
+    /*
+    *Sum two binary strings.
+    * @parmas a, first binary string to add
+    * @params b, second binary string to add
+    * @return binary sum of a and b
+    */
     public String addBinary(String a, String b) {
         //convert strings into binary numbers
         //in order to fix a runtime error for large values convert int to BigInteger
