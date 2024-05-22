292be9ce18fa3e0147739f8fd2051a1a148e87e3
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 08:45:53 -0400

    Update Remove_Element.java
    Updated the documentation comment code to be more detailed.

--- Remove_Element.java
+++ Remove_Element.java
@@ -1,4 +1,25 @@
+/*
+* Leetcode Challenge: Remove Element
+*
+* Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+* The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+* Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+* Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+* The remaining elements of nums are not important as well as the size of nums.
+* Return k.
+*
+* 5/14/24
+* Author: Mark Robuck
+*
+*/
+
 class Solution {
+    /*
+    * Remove all occurrences of a value from an array
+    * @param nums, integer array of numbers to be checked for val
+    * @param vam, integer to be checked for in nums and removed
+    * @return integer value representing the number of elements in nums not equal to val
+    */
     public int removeElement(int[] nums, int val) {
         //create left and right pointers
         int left = 0;
