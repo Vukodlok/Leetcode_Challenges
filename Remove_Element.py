c5aba8d9f226e0dc0cc864c964de1919ba77a629
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 08:46:07 -0400

    Update Remove_Element.py
    Updated the documentation comment code to be more detailed.

--- Remove_Element.py
+++ Remove_Element.py
@@ -1,3 +1,17 @@
+'''
+# Leetcode Challenge: Remove Element
+#
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+# The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+# Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+# Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+# The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+#
+# 5/14/24
+# Author: Mark Robuck
+#
+'''
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         #create left and right pointers
