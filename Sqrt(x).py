b31f5db9a4da79d7a25616f7debe7712fb5fa6b0
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 12:49:16 -0400

    Update Sqrt(x).py
    Update documentation comment code.

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -1,8 +1,25 @@
+"""
+Leetcode Challenge: Sqrt(x)
+
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+You must not use any built-in exponent function or operator.
+
 #I remember being assigned this problem in a college class way back when.
 #I don't remember how we solved it but I think we used a summation and/or something
 #called Newton's Algorithm or Newton's Sum or something along those lines to approximate
 #a value and then check how close it was in order to make closer and closer guesses.
+
+5/8/24
+Mark Robuck
+"""
 class Solution:
+    """
+    approximate the square root of a number, rounded down
+    Args:
+        x(int): number to be rooted
+    Returns:
+        int: approximated root of x rounded down to the nearest integer
+    """
     def mySqrt(self, x: int) -> int:
         root = x
         #loop enough times for a reasonable answer given values within the range
