407362776dea72b8fa9bf06b72b9d69fa30e5f50
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 10:08:01 -0400

    Update Roman_to_Integer.py
    Updated documentation comment code.

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -1,4 +1,30 @@
+"""
+Leetcode Challenge: Roman to Integer
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+Given a roman numeral, convert it to an integer.
+
+4/26/24
+Mark Robuck
+"""
 class Solution:
+    """
+    Take a string representing a roman numeral and convert it to an integer value
+    Args:
+        s(str): roman numeral as a string
+    Returns:
+        int: roman numeral as an integer value
+    """
     def romanToInt(self, s: str) -> int:
         #create mapping of roman numerals to integer values
         romanValues = {'I':1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
