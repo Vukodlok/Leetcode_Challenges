19d0a1ff3f0c929a1bc0a361da0950e94531802b
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 10:13:52 -0400

    Update Length_of_Last_Word.py
    Updated documentation comment code.

--- Length_of_Last_Word.py
+++ Length_of_Last_Word.py
@@ -1,5 +1,20 @@
+"""
+Leetcode Challenge: Length of Last Word
+
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+4/27/24
+Mark Robuck
+"""
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        """
+        return the length of the last word in a string
+        Args:
+            s(str): given string
+        Returns:
+            length of the last word in the string as an integer
+        """
         #split the string into 'words' and store in an array
         words = s.split()
         #grab the last index in the array
