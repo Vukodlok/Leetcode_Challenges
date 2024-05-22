eb33dc8518bd89bb2a1b6d5fb570a5cad1068956
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 09:18:54 -0400

    Update Remove_Duplicates_from_Sorted_Array.java
    Updated documentation comment code.

--- Remove_Duplicates_from_Sorted_Array.java
+++ Remove_Duplicates_from_Sorted_Array.java
@@ -1,4 +1,23 @@
+/*
+* Leetcode Challenge: Remove Duplicates from Sorted Array
+* 
+* Given an integer array nums sorted in non-decreasing order, 
+* remove the duplicates in-place such that each unique element appears only once. 
+* The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+* Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+* Change the array nums such that the first k elements of nums contain the unique elements in the order they were
+* present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+* Return k.
+*
+* 5/15/24
+* Mark Robuck
+*/
 class Solution {
+    /*
+    * Remove all duplicate values from the sorted array
+    * @params nums, list of integers in ascending order
+    * @return integer length of list after duplicates are removed
+    */
     public int removeDuplicates(int[] nums) {
         //create two pointers for start and end of the array
         int left = 0;
