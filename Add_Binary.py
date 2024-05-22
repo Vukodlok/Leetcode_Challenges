68f790735e75d0db2f8657ea1b0154ae5edba1b3
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 12:21:30 -0400

    Update Add_Binary.py
    Update documentation comment code.

--- Add_Binary.py
+++ Add_Binary.py
@@ -1,4 +1,21 @@
+"""
+Leetcode Challenge: Add Binary
+
+Given two binary strings a and b, return their sum as a binary string.
+
+4/29/24
+Mark Robuck
+"""
+
 class Solution:
+    """
+    Sum two binary strings.
+    Args:
+        a(str): first binary string to add
+        b(str): second binary string to add
+    Returns:
+        str: binary sum of a and b
+    """
     def addBinary(self, a: str, b: str) -> str:
         #convert strings to binary using integer casting of base 2
         binaryA = int(a, 2)
