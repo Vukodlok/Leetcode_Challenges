b7f907f847d460fa60f41a6e18c815cf2f36ad8a
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-05-22 12:49:23 -0400

    Update Sqrt(x).java
    Update documentation comment code.

--- Sqrt(x).java
+++ Sqrt(x).java
@@ -1,7 +1,24 @@
-/*I solved this in Python first and used my solution for Java.
-In order to pass all the unit tests I needed to make sure to use double values
-instead of int on lines 6 and 13.  Otherwise the way Python vs Java handles values and rounded caused the unit tests to fail in Java where they passed in Python. */
+/*
+* Leetcode Challenge: Sqrt(x)
+* 
+* Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+* You must not use any built-in exponent function or operator.
+*
+* I solved this in Python first and used my solution for Java.
+* In order to pass all the unit tests I needed to make sure to use double values
+* instead of int on lines 6 and 13.  Otherwise the way Python vs Java handles values 
+* and rounded caused the unit tests to fail in Java where they passed in Python.
+*
+* 5/8/24
+* Mark Robuck
+*/
+
 class Solution {
+    /*
+    * approximate the square root of a number, rounded down
+    * @params x, number to be rooted
+    * @return approximated root of x rounded down to the nearest integer
+    */
     public int mySqrt(int x) {
         double root = x;
         //loop enough times for a reasonable answer given values within the range
