34fd32b4ddf715c51aa6657df79c0b731c520177
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-06-23 12:41:03 -0400

    Create Remove_Duplicates_from_Sorted_Array_II.py
    Python solution for Remove Duplicates from Sorted Array II challenge on Leetcode.com.

---
+++ Python_Solutions/Remove_Duplicates_from_Sorted_Array_II.py
@@ -0,0 +1,48 @@
+"""
+Leetcode Challenge: Remove Duplicates from Sorted Array II
+
+Given an integer array nums sorted in non-decreasing order, remove some duplicates 
+in-place such that each unique element appears at most twice. The relative order 
+of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must 
+instead have the result be placed in the first part of the array nums. More formally, 
+if there are k elements after removing the duplicates, then the first k elements of nums 
+should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+Do not allocate extra space for another array. You must do this by modifying 
+the input array in-place with O(1) extra memory.
+
+6/23/24
+Mark Robuck
+"""
+class Solution:
+    """
+    Remove duplicates in a list of numbers without moving the order of the elements.
+    Args: 
+        nums(List[int]): original list of integers
+    Returns:
+        int: the number of elements in the list after duplicates are removed
+    """
+    def removeDuplicates(self, nums: List[int]) -> int:
+        #check for empty list
+        if not nums:
+            return 0
+            
+        #create pointers for modifying a value and checking a value
+        write_pointer = 1
+        #track a counter for each value
+        count = 1
+
+        #traverse the list
+        for read_pointer in range(1, len(nums)):
+            #if duplicate values add to the counter, otherwise reset counter
+            if nums[read_pointer] == nums[read_pointer - 1]:
+                count += 1
+            else: count = 1
+            #if 2 or less duplicates keep in the list
+            if count <= 2:
+                nums[write_pointer] = nums[read_pointer]
+                write_pointer += 1   
+        return write_pointer
