5dcfef1167958600048dc23c2b5f19239ddb5914
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-09-07 17:13:12 -0400

    Update Design_Add_and_Search_Words_Data_Structure.py

--- Python_Solutions/Design_Add_and_Search_Words_Data_Structure.py
+++ Python_Solutions/Design_Add_and_Search_Words_Data_Structure.py
@@ -7,7 +7,9 @@ Implement the WordDictionary class:
 
 WordDictionary() Initializes the object.
 void addWord(word) Adds word to the data structure, it can be matched later.
-bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+bool search(word) Returns true if there is any string in the data structure that 
+matches word or false otherwise. word may contain dots '.' where dots can be matched 
+with any letter.
 
 9/7/24
 Mark Robuck
