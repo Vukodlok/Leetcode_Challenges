f13fdb42ba84c005ea83319e98c012c52329468c
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-09-23 10:41:34 -0400

    Create Factorial_Trailing_Zeroes.py
    Python solution to Factorial Trailing Zeroes challenge in Leetcode.com.

---
+++ Python_Solutions/Factorial_Trailing_Zeroes.py
@@ -0,0 +1,33 @@
+"""
+Leetcode Challenge: Factorial Trailing Zeroes
+
+Given an integer n, return the number of trailing zeroes in n!.
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+
+9/23/24
+Mark Robuck
+"""
+
+class Solution:
+    """
+    Return the number of trailing zeroes in a factorial calculation.
+    Args:
+        n(int): number to take the factorial of
+    Returns:
+        int: number of trailing zeroes in n!
+    """
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        #count how many times 5 is a factor in the numbers from 1 to n
+        while n >= 5:
+            n //= 5
+            count += n
+        return count
+        
+
+"""
+A trailing zero is created by multiplying 10, which is the result 
+of multiplying 2 and 5. In any factorial, there are always more 
+factors of 2 than 5, so the number of trailing zeroes is determined 
+by how many times 5 is a factor in the numbers from 1 to n.
+"""
